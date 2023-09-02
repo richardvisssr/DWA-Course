4689cbeb04f3f8818185155604984787c1df3b01
Richard Visser <HJ.Visser@student.han.nl>
2023-09-02 12:00:15 +0200

    Oefening 11.3.3

--- SWD/account_management/YOUR_ANSWER.js
+++ SWD/account_management/YOUR_ANSWER.js
@@ -1,4 +1,18 @@
-fs = require('fs');
+const fs = require('fs');
+const path = require('path')
 
+// fs.readdir('accounts', (err, fileList) => {
+//     console.log(fileList);
+//     console.log(fileList.length);
+// });
+
+fs.readFile(path.join('accounts', 'serena_klein'), (err, fileContents) => {
+    //Node uses a special data type (called buffer) to store the contents of a file 
+    //(for performance reasons)
+    //By calling toString() we can convert this type to a human readble string
+    console.log(fileContents.toString());
+});
+
+fs.readFile;
 // Get an array of all files in the folder: 'accounts' using fs.readdir
 // Log the contents of the last file in the list to the console using fs.readFile
