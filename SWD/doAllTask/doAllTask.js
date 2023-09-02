9d5b2fea5db28572fd13e70cbd83ad886232a5db
Richard Visser <HJ.Visser@student.han.nl>
2023-09-02 12:41:34 +0200

    doAllTask

---
+++ SWD/doAllTask/doAllTask.js
@@ -0,0 +1,44 @@
+/* A */
+
+// let doAllTasks = (printWhenFinished) => {
+//     setTimeout(() => {
+//         console.log('Taak 1 klaar');
+//         setTimeout(() => {
+//             console.log('Taak 2 klaar');
+//             printWhenFinished();
+//         }, Math.random() * 100);
+//     }, Math.random() * 100);    
+// };
+
+// /* B */
+
+// let printWhenFinished = () => {
+//     console.log('Alle taken klaar');
+//     console.log('nu gaan we andere dingen doen');
+// };
+
+// doAllTasks(printWhenFinished);
+
+/* C */
+
+let doAllTasks2 = (taskDone) => {
+    let completedTasks = [];
+    setTimeout(() => {
+        completedTasks.push('Taak 1 klaar');
+        setTimeout(() => {
+            completedTasks.push('Taak 2 klaar');
+            taskDone(completedTasks);
+        }, Math.random() * 100);
+    }, Math.random() * 100);    
+}; 
+
+
+/* D */
+let printResults = (resultList) => {
+    console.log('Alle taken klaar, dit zijn de resultaten');
+    resultList.forEach((result) => {
+        console.log(result);
+    });
+};
+
+doAllTasks2(printResults);
