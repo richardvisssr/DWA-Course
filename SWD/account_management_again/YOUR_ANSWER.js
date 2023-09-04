6d3552a042b7e2c45e98c0fa57d0bd38f0123912
Richard Visser <HJ.Visser@student.han.nl>
2023-09-04 16:21:57 +0200

    opdracht account managment again

---
+++ SWD/account_management_again/YOUR_ANSWER.js
@@ -0,0 +1,22 @@
+const pw = require('./promise-wrappers');
+
+users = [
+    {
+        "account": "ashlee_waters",
+        "username": "ASH"
+    },
+    {
+        "account": "hilario_muller",
+        "username": "Hilario_Muller29"
+    },
+    {
+        "account": "serena_klein",
+        "username": "Serena.Klein"
+    }
+];
+
+pw.writeFileP(Promise.all([users]).then(() => {
+    console.log('done');
+}).catch(err => {
+    console.log(err.message);
+})); 
