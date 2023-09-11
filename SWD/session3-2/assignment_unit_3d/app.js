28a0edd5b3c5ef54087797fd607e7b01cbb9b410
Richard Visser <HJ.Visser@student.han.nl>
2023-09-11 14:48:30 +0200

    3.3 toegevoegd

--- SWD/session3-2/assignment_unit_3d/app.js
+++ SWD/session3-2/assignment_unit_3d/app.js
@@ -12,39 +12,31 @@ const gameFilesFolderName = 'game_files';
 
 app.use(bodyParser.json());
 
+const gameFileReader = async (req, res, next) => {
+  //TODO D1, D2
+} 
+
+app.use(/* TODO D1 */);
+
+
+const gameStateReader; //TODO D3
+
 app.get('/action/:player/where', async (req, res) => {
     const fileName = path.join(gameFilesFolderName, `${req.params.player}.json`);
     const fileContent = await promiseWrappers.readFileP(fileName);
-    
     const gameState = JSON.parse(fileContent);
     const game = new Game(gameState);
-    
     const locationInformation = await game.getLocationInformation();
     res.json(locationInformation);
 });
 
 
 app.post('/action/:player/goto', async (req, res) => {
-    try {
-        const player = req.params.player;
-        const locationName = req.params.location; // Haal de locatie uit de routeparameter
-        const game = new Game(); // Maak een nieuw Game-object aan
-
-        // Roep de goToLocation-functie van de Game-klasse aan om de locatie te wijzigen
-        const locationDescription = await game.goToLocation(locationName);
-
-        // Roep getLocationInformation-functie aan om informatie over de nieuwe locatie op te halen
-        const locationInformation = await game.getLocationInformation(player);
-
-        // Stuur de beschrijving van de nieuwe locatie als JSON-respons
-        res.json(locationInformation);
-    } catch (error) {
-        res.status(500).json({ error: 'Er is een fout opgetreden bij het verplaatsen naar de nieuwe locatie.' });
-    }
+   //Paste your implementation from assignment unit 3c here
 });
 
 app.post('/action/:player/arise', async (req, res) => {
-  
+  //Paste your implementation from assignment unit 3c here
 });
 
 const server = app.listen(3000, () => {
