017fa7db8ff49cb0d16cf245023891ec494f9885
Richard Visser <HJ.Visser@student.han.nl>
2023-09-12 17:21:56 +0200

    3d

--- SWD/session3-2/assignment_unit_3d/app.js
+++ SWD/session3-2/assignment_unit_3d/app.js
@@ -14,34 +14,62 @@ app.use(bodyParser.json());
 
 const gameFileReader = async (req, res, next) => {
   //TODO D1, D2
- try {
   const fileName = path.join(gameFilesFolderName, `${req.params.player}.json`);
-  const fileContent = await promiseWrappers.readFileP(fileName);
-  req.gameState = JSON.parse(fileContent);
-  next();
-} catch (error) {
-  res.status(500).json({ error: 'something horrible happened' });
+  try{
+  res.gameFileReader = await promiseWrappers.readFileP(fileName);
+  if(res.gameFileReader !== undefined || res.gameFileReader !== null){
+    
+      next();
+  } 
+  } catch (error) {
+    return next(error);
 }
-} 
-
-app.use(gameFileReader);
+};
 
 
-const gameStateReader; //TODO D3
+app.use('/action/:player', gameFileReader);
+
+const errHandler = async (err, req, res, next) => {
+ if (err.code === 'ENOENT' && err.syscall === 'open') {
+    res.status(404).json({ error: 'Game not found.' });
+  } else {
+    next(err);
+  }
+}; 
+
+app.use('/action/:player', errHandler);
+
+const gameStateReader = async (req, res, next) => {
+  const gameState = JSON.parse(res.gameFileReader);
+  res.game = new Game(gameState);
+  res.json(res.game);
+  //?????????????????????????
+  next();
+};
+
+app.use('/action/:player', gameStateReader);
 
 app.get('/action/:player/where', async (req, res) => {
-  const game = new Game(req.gameState);
-  const locationInformation = await game.getLocationInformation();
-  res.json(locationInformation);
+    const locationInformation = await game.getLocationInformation();
+    res.json(locationInformation);
 });
 
 
 app.post('/action/:player/goto', async (req, res) => {
-   //Paste your implementation from assignment unit 3c here
+  const locationInformation = await game.getLocationInformation();
+  res.json(locationInformation);
 });
 
 app.post('/action/:player/arise', async (req, res) => {
-  //Paste your implementation from assignment unit 3c here
+  try {
+      const game = new Game();
+      const body = req.body;
+      const newGame = await game.startNew(body.startLocation, body.inventory);
+      res.json(newGame);
+  } catch (error) {
+      res.status(500).json({ error: 'Er is een fout opgetreden.' });
+  }
+
 });
 
 const server = app.listen(3000, () => {
