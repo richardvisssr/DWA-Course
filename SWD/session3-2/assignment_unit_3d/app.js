1419485f667c6b64e91bfd53a360bc838d45ad9a
Richard Visser <HJ.Visser@student.han.nl>
2023-09-14 17:13:12 +0200

    3D

--- SWD/session3-2/assignment_unit_3d/app.js
+++ SWD/session3-2/assignment_unit_3d/app.js
@@ -17,7 +17,7 @@ const gameFileReader = async (req, res, next) => {
   const fileName = path.join(gameFilesFolderName, `${req.params.player}.json`);
   try {
     res.gameFileReader = await promiseWrappers.readFileP(fileName);
-    if (res.gameFileReader !== undefined || res.gameFileReader !== null) {
+    if (res.gameFileReader !== undefined && res.gameFileReader !== null) {
       next();
     }
   } catch (error) {
@@ -27,34 +27,37 @@ const gameFileReader = async (req, res, next) => {
 
 app.use("/action/:player", gameFileReader);
 
-const errHandler = async (err, req, res, next) => {
+const errHandler = (err, req, res, next) => {
   if (err.code === "ENOENT" && err.syscall === "open") {
     res.status(404).json({ error: "Game not found." });
   } else {
-    next(err);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
+
+const gameStateReader = async (req, res, next) => {
+  try {
+    const gameState = JSON.parse(res.gameFileReader);
+    res.game = new Game(gameState);
+    next();
+  } catch (error) {
+    next(error); // Pass errors to the default error handler
   }
 };
 
 app.use("/action/:player", errHandler);
 
-const gameStateReader = async (req, res, next) => {
-  const gameState = JSON.parse(res.gameFileReader);
-  res.game = new Game(gameState);
-  res.json(res.game);
-  //?????????????????????????
-  next();
-};
-
-app.use("/action/:player", gameStateReader);
-
-app.get("/action/:player/where", async (req, res) => {
-  const locationInformation = await game.getLocationInformation();
+app.get("/action/:player/where", gameStateReader, async (req, res) => {
+  const locationInformation = await res.game.getLocationInformation();
   res.json(locationInformation);
 });
 
-app.post("/action/:player/goto", async (req, res) => {
-  const locationInformation = await game.getLocationInformation();
-  res.json(locationInformation);
+app.post("/action/:player/goto", gameStateReader ,async (req, res) => {
+  const locationName = req.query.location; 
+  const locationDescription = await res.game.goToLocation(locationName);
+  // const locationInformation = await res.game.goToLocation(req.query.player);
+  res.json(locationDescription);
 });
 
 app.post("/action/:player/arise", async (req, res) => {
